3be31fe74064daf1dcce14810e905fd8568c34e8
Abu Saleh Muhammad Shaon <srabon.php@gmail.com>
2021-07-24 02:42:46 +0600

    Q1 - optimize listing record page using query pagination

--- app/Config/database.php
+++ app/Config/database.php
@@ -71,10 +71,10 @@ class DATABASE_CONFIG {
 	public $default = array(
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
-		'host' 		 => '',
-		'login'      => '',
+		'host' 		 => '127.0.0.1',
+		'login'      => 'root',
 		'password'   => '',
-		'database'   => '',
+		'database'   => 'trial',
 		'prefix'     => '',
 		'encoding'   => 'utf8',
 	);
--- app/Controller/RecordController.php
+++ app/Controller/RecordController.php
@@ -1,16 +1,53 @@
 <?php
 	class RecordController extends AppController{
-		
-		public function index(){
-			ini_set('memory_limit','256M');
-			set_time_limit(0);
-			
+
+        public function index()
+        {
 			$this->setFlash('Listing Record page too slow, try to optimize it.');
-			
-			
-			$records = $this->Record->find('all');
-			
-			$this->set('records',$records);
+
+            if($this->request->is('post')) {
+                $limit = $this->request->data('iDisplayLength');
+                $offset = $this->request->data('iDisplayStart');
+                $searchKey = $this->request->data('sSearch');
+
+                $conditions = array();
+                $orCondition = array();
+
+                if(empty($searchKey) === false) {
+                    $searchKey = trim($searchKey);
+                    $orCondition[]['id LIKE'] = '%' . $searchKey . '%';
+                    $orCondition[]['name LIKE'] = '%' . $searchKey . '%';
+
+                    $conditions += [
+                        'OR' => $orCondition
+                    ];
+                }
+
+                $records = $this->Record->find('all', array('conditions' => $conditions,'limit' => $limit, 'offset' => $offset));
+
+                $count = $this->Record->find('count', [
+                    'conditions' => $conditions
+                ]);
+
+                $responseData = array(
+                    'draw' => $this->request->data('sEcho'),
+                    'iTotalDisplayRecords' => $count,
+                    'iTotalRecords' => $count,
+                    'aaData' => []
+                );
+
+                foreach ($records as $key => $record) {
+                    $responseData['aaData'][$key] = array(
+                        $record['Record']['id'],
+                        $record['Record']['name']
+                    );
+                }
+
+                $result = json_encode($responseData, JSON_PRETTY_PRINT);
+                $this->response->body($result);
+
+                return $this->response;
+            }
 			
 			
 			$this->set('title',__('List Record'));
